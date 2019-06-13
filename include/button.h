04419aa58c339db0cb4d3bf2e61cf6502b67b6b4
Henry John Krumb <henry.krumb@computerwerk.org>
2019-06-13 13:57:49 +0200

    implement button behavior

--- include/button.h
+++ include/button.h
@@ -5,7 +5,15 @@
 
 class Button: public Widget {
 public:
-	Button(std::string label, std::string address="");
+	Button(std::string label, bool center=true, std::string address="");
 	Button(const Button&) = delete;
 	virtual ~Button() {}
+
+	int _handle_key(const int& key);
+	void _show(Canvas& canvas);
+
+
+protected:
+	std::string m_label;
+	bool m_center;
 };
--- src/button.cpp
+++ src/button.cpp
@@ -1,2 +1,56 @@
-#include "button.h"
+#include <sstream>
 
+#include "button.h"
+#include "geometry.h"
+#include "utils.h"
+
+
+Button::Button(std::string label, bool center, std::string address):
+		Widget(address),
+		m_label(label),
+		m_center(center)
+{
+	MAKE_ADDRESS(Button);
+	m_focusable = true;
+}
+
+
+int Button::_handle_key(const int& key) {
+	if (key == ' ') {
+		send_event("activate");
+		return 0;
+	}
+	return key;
+}
+
+
+void Button::_show(Canvas& canvas) {
+	canvas.draw_frame(m_box);
+	int x = m_box.x(), y = m_box.y(), w = m_box.w(), h = m_box.h();
+	std::stringstream sstr;
+	
+	if (m_focused) {
+		sstr << ">> ";
+	}
+	else {
+		sstr << "   ";
+	}
+	sstr << "[" << m_label << "]";
+	std::string text = sstr.str();
+	
+	Dimension d = stringsize(text);
+	if (m_center) {
+		canvas.draw_text(
+			text,
+			x + (w - d.w()) / 2,
+			y + (h - d.h()) / 2
+		);
+	}
+	else {
+		canvas.draw_text(
+			text,
+			x,
+			y + (h - d.h()) / 2
+		);
+	}
+}

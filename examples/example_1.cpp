e862ef219479f91945c8978ad8c3e07370cff030
Henry John Krumb <henry.krumb@computerwerk.org>
2019-06-13 13:27:57 +0200

    enhance example code

--- examples/example_1.cpp
+++ examples/example_1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "../include/stewie++.h"
 
 #include "example_1.h"
@@ -5,6 +7,19 @@
 
 int example_1(int argc, char* argv[]) {
 	Application app;
+	
+	auto frame = std::make_shared<Frame>();
+	auto vbox = std::make_shared<VBox>();
+	
+	auto chk_filter_ac_wide = std::make_shared<CheckBox>("ac wide");
+	vbox->add_child(std::dynamic_pointer_cast<Widget>(chk_filter_ac_wide));
+	
+	auto chk_filter_ac_narrow = std::make_shared<CheckBox>("ac narrow");
+	vbox->add_child(std::dynamic_pointer_cast<Widget>(chk_filter_ac_narrow));
+	
+	frame->add_child(std::dynamic_pointer_cast<Widget>(vbox));
+	
+	app.add_frame(frame);
 	
 	app.run();
 	
--- examples/main.cpp
+++ examples/main.cpp
@@ -3,10 +3,32 @@
 #include "example_1.h"
 
 
+void usage(char* argv0) {
+	std::cout << "usage: " << std::endl;
+	std::cout << argv0 << " <EXAMPLE>" << std::endl;
+	std::cout << std::endl;
+	std::cout << "EXAMPLEs are:" << std::endl;
+	std::cout << "1" << std::endl;
+}
+
+
 int main(int argc, char* argv[]) {
 	int status;
 	
-	status = example_1(argc, argv);
+	if (argc != 2) {
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	
+	std::string example(argv[1]);
+	
+	if (example == "1") {
+		status = example_1(argc, argv);
+	}
+	else {
+		std::cout << "example '" << example << "' not available" << std::endl;
+		status = EXIT_FAILURE;
+	}
 	
 	return status;
 }

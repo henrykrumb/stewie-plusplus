decf37a138ca57a575a09a664d2b87ad5187062f
Henry John Krumb <henry.krumb@computerwerk.org>
2019-06-16 15:16:23 +0200

    implement progressbar show method

--- include/progressbar.h
+++ include/progressbar.h
@@ -1,17 +1,28 @@
 #pragma once
-
+#include "evaluatable.h"
 #include "widget.h"
 
 
-class ProgressBar: public Widget {
+#define EVENT_SET_PROGRESS "set progress"
+
+
+class ProgressBar: public Widget, public Evaluatable {
 public:
-	ProgressBar(std::string address="");
+	ProgressBar(std::string label, std::string address="");
 	ProgressBar(const ProgressBar&) = delete;
 	virtual ~ProgressBar();
-
+	
 	void set_progress(const float& progress);
+	
+	virtual void handle_event(const Event& event);
+	
+	virtual void _show(Canvas& canvas);
+	virtual int _handle_key(const int& key);
+	
+	virtual EvalVariant evaluate();
 
 
 protected:
+	std::string m_label;
 	float m_progress;
 };
--- src/progressbar.cpp
+++ src/progressbar.cpp
@@ -1,10 +1,18 @@
+#include <sstream>
+
 #include "progressbar.h"
+#include "utils.h"
 
 
-ProgressBar::ProgressBar(std::string address):
-		Widget(address)
+ProgressBar::ProgressBar(std::string label, std::string address):
+		Widget(address),
+		Evaluatable(label),
+		m_label(label)
 {
 	MAKE_ADDRESS(ProgressBar);
+	if (m_name.empty()) {
+		m_name = m_address;
+	}
 	m_focusable = false;
 }
 
@@ -19,4 +27,58 @@ void ProgressBar::set_progress(const float& progress) {
 		throw std::invalid_argument("invalid progress value");
 	}
 	m_progress = progress;
+}
+
+
+void ProgressBar::handle_event(const Event& event) {
+	if (event.get_id() == EVENT_SET_PROGRESS) {
+		try {
+			set_progress(std::get<float>(event.get_data()));
+		} catch (const std::bad_variant_access& bva) {
+			
+		}
+	}
+	else {
+		
+	}
+}
+
+
+void ProgressBar::_show(Canvas& canvas) {
+	canvas.draw_frame(m_box);
+	int x = m_box.x(), y = m_box.y(), w = m_box.w(), h = m_box.h();
+	
+	std::stringstream sstr;
+	
+	int percentage = (int) (100.0f * m_progress);
+	int w_total = (int)(w / 2.0f - 2.0f);
+	int w_full = (int)((float) w_total * m_progress);
+	int w_empty = (int)((float) w_total * (1.0f - m_progress));
+	
+	sstr << "[";
+	for (int i = 0; i < w_full; i++) {
+		sstr << "#";
+	}
+	for (int i = 0; i < w_empty; i++) {
+		sstr << " ";
+	}
+	sstr << "] " << percentage << "%";
+	std::string text = sstr.str();
+	
+	Dimension d = stringsize(text);
+	canvas.draw_text(
+		text,
+		x + (w - d.w()) / 2,
+		y + (h - d.h()) / 2
+	);
+}
+
+
+int ProgressBar::_handle_key(const int& key) {
+	return key;
+}
+
+
+EvalVariant ProgressBar::evaluate() {
+	return EvalVariant(m_progress);
 }
